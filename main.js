494ff4edc0c125538e2a6cf9b067a720a23f5fb1
ChrisMichael123 <87139501+ChrisMichael123@users.noreply.github.com>
2022-03-29 14:09:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+function setup(){
+canvas= createCanvas(300,300);
+canvas.center();
+background("white");
+}
+function draw(){
+
+}
+function Clear(){
+    background("white");
+    
+}
